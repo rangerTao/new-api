d0da0464a9b005ba88b890b9db38c7c99771aab1
taoliang1 <taoliang1985@gmail.com>
2026-06-22 16:19:54 +0800

    Update embed-file-system.go

--- common/embed-file-system.go
+++ common/embed-file-system.go
@@ -59,16 +59,24 @@ type themeAwareFileSystem struct {
 
 func (t *themeAwareFileSystem) Exists(prefix string, path string) bool {
 	if GetTheme() == "classic" {
-		return t.classicFS.Exists(prefix, path)
+		return t.classicFS.Exists(prefix, path) || t.defaultFS.Exists(prefix, path)
 	}
-	return t.defaultFS.Exists(prefix, path)
+	return t.defaultFS.Exists(prefix, path) || t.classicFS.Exists(prefix, path)
 }
 
 func (t *themeAwareFileSystem) Open(name string) (http.File, error) {
 	if GetTheme() == "classic" {
-		return t.classicFS.Open(name)
+		file, err := t.classicFS.Open(name)
+		if err == nil {
+			return file, nil
+		}
+		return t.defaultFS.Open(name)
 	}
-	return t.defaultFS.Open(name)
+	file, err := t.defaultFS.Open(name)
+	if err == nil {
+		return file, nil
+	}
+	return t.classicFS.Open(name)
 }
 
 func NewThemeAwareFS(defaultFS, classicFS static.ServeFileSystem) static.ServeFileSystem {
